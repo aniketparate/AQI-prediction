d3bf43f48203a665d4bc745da54d7db105c53e4a
Aniket Parate <aniketvparate@gmail.com>
2023-06-26 20:52:59 +0530

    added output_image

--- code/conversion.py
+++ code/conversion.py
@@ -10,34 +10,34 @@ def find_closest_rgb(rgb, rgb_values):
     closest_rgb = None
     closest_distance = float('inf')
     for rgb_value in rgb_values:
-        if rgb_value == [-1, -1, -1]:
+        if rgb_value[:3] == [-1, -1, -1]:
             closest_rgb = [-1, -1, -1]
             return closest_rgb
-        distance = calculate_distance(rgb, rgb_value)
+        distance = calculate_distance(rgb, rgb_value[:3])
         if distance < closest_distance:
             closest_distance = distance
             closest_rgb = rgb_value
     return closest_rgb
 
-# Read the RGB values from the first CSV file
-with open('first_csv_file.csv', 'r') as file:
+# Read the RGB and temperature values from the first CSV file
+with open('rgb_temperature_values.csv', 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row if it exists
-    rgb_values = [list(map(int, row[:3])) for row in reader]
+    rgb_temperature_values = [list(map(int, row[:4])) for row in reader]
 
-# Read the RGB values from the second CSV file
-with open('second_csv_file.csv', 'r') as file:
+# Read the X, Y, r, g, b values from the second CSV file
+with open('temperature_data.csv', 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row if it exists
-    rgb_values_to_update = [list(map(int, row[:3])) for row in reader]
+    temperature_data = [list(map(int, row[:5])) for row in reader]
 
-# Iterate over the RGB values in the second file and find the closest RGB value from the first file
-for i, rgb_value_to_update in enumerate(rgb_values_to_update):
-    closest_rgb = find_closest_rgb(rgb_value_to_update, rgb_values)
-    rgb_values_to_update[i] = closest_rgb
+# Iterate over the temperature data and find the closest RGB value from the first file
+for i, data in enumerate(temperature_data):
+    closest_rgb = find_closest_rgb(data[2:5], rgb_temperature_values)
+    temperature_data[i][2:5] = closest_rgb[:3]
 
-# Write the updated RGB values to a new CSV file
+# Write the updated X, Y, r, g, b values to a new CSV file
 with open('updated_csv_file.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['R', 'G', 'B'])
-    writer.writerows(rgb_values_to_update)
+    writer.writerow(['X', 'Y', 'r', 'g', 'b'])
+    writer.writerows(temperature_data)
--- code/recolor.py
+++ code/recolor.py
@@ -1,31 +1,29 @@
-import csv
 import cv2
 import numpy as np
+import pandas as pd
 
-# Read the CSV file containing pixel color values
-csv_path = '2000_LST.csv'
+# Read the CSV file
+data = pd.read_csv('updated_csv_file.csv')
 
-rows = []
-with open(csv_path, 'r') as file:
-    reader = csv.reader(file)
-    next(reader)  # Skip the header
-    for row in reader:
-        rows.append(row)
+# Get the maximum row and column values
+max_row = data['Y'].max()
+max_column = data['X'].max()
 
-# Determine the dimensions of the image
-max_row = max(int(row[0]) for row in rows)
-max_col = max(int(row[1]) for row in rows)
+# Create a blank image with the size based on the maximum row and column values
+image = np.zeros((max_row + 1, max_column + 1, 3), dtype=np.uint8)
 
-# Create an empty image with original color dimensions
-reconstructed_image = np.zeros((max_row + 1, max_col + 1, 3), dtype=np.uint8)
+# Iterate over the rows of the CSV data
+for index, row in data.iterrows():
+    r, c, red, green, blue = row['Y'], row['X'], row['r'], row['g'], row['b']
+    image[r, c] = (blue, green, red)  # Set the pixel value in the image
 
-# Set pixel color values based on the CSV data
-for row in rows:
-    r, c, b, g, r = map(int, row)  # Assuming CSV format: "Row", "Column", "Blue", "Green", "Red"
-    reconstructed_image[r, c] = (b, g, r)
+# Display the image
+# cv2.imshow('Image', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 # Save the reconstructed image
 output_image_path = "output_image.bmp"
-cv2.imwrite(output_image_path, reconstructed_image)
+cv2.imwrite(output_image_path, image)
 
 print("Image saved successfully.")
